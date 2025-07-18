51cfcda96049b0fa735b1d158d462f6ebcad46b6
hemanthgopisetty <hemanth.508136@gmail.com>
2025-07-18 13:19:15 +0530

    Update the pointer and loop condition based on the nature of the question

--- 7.SearchInsertPosition.cpp
+++ 7.SearchInsertPosition.cpp
@@ -1,5 +1,5 @@
 /**
- * Given Sorted array , of distinct integers 
+ * Given Sorted array , of distinct integers
  * and a target value , return the index if the target is found
  * if the target is not found  return the index where the target can be inserted
  * in the sorted array in order
@@ -11,14 +11,17 @@ class Solution
 public:
     int searchInsert(vector<int> &nums, int target)
     {
-        int low=0,high=nums.size()-1;
+        int low = 0, high = nums.size() - 1;
 
-        while(low<=high)
+        while (low <= high)
         {
-            int mid = low+(high-low)/2;
-            if(nums[mid]==target)return mid;
-            else if(nums[mid]<target)low=mid+1;
-            else high=mid-1;
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
         return low;
     }
@@ -34,19 +37,22 @@ int main()
 
 /**
  * Find the value in the sorted array if found return the index
- * and if it's not found 
+ * and if it's not found
  * then search the value where target can be inserted
- * 
+ *
  * from this question what we learn
- * how to set 
- *  loop condtion 
- *  and update the pointer's 
+ * how to set
+ *  loop condtion
+ *  and update the pointer's
  *  by m+1 or m-1 or m
- * 
- * 
- * We have to update this by the nature of the question 
- * 
+ *
+ *
+ * We have to update this by the nature of the question
+ *
  * search for mid , if found
- * else return left since 
- *  
+ * else return left since
+ *
+ *
+ *
+ * learning : Update the pointer and loop condition based on the nature of the question
  */
